8d90fa50826514bb5065c586b965a5a4ac5e7491
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-04-27 20:37:14 +0300

    Added parsing of filename from command line

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,3 +1,6 @@
+import argparse
+
+
 def load_data(filepath):
     pass
 
@@ -6,8 +9,24 @@ def get_most_frequent_words(text):
     pass
 
 
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'filename',
+        help='a text file for analysis',
+        type=str,
+    )
+
+    command_line_arguments = parser.parse_args()
+
+    return command_line_arguments
+
+
 def main():
-    pass
+    command_line_arguments = parse_command_line_arguments()
+
+    filename = command_line_arguments.filename
 
 
 if __name__ == '__main__':
